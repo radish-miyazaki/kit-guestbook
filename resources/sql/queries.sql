88fec3fca63a8425899de26488cec7789fbde0cc
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-02-16 14:39:47 +0900

    add some queries

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,1 +1,8 @@
--- Place your queries here. Docs available https://www.hugsql.org/
+-- :name save-message! :! :n
+-- :doc creates a new message
+INSERT INTO guestbook (name, message) VALUES (:name, :message)
+
+-- :name get-messages :? :*
+-- :doc select all avaiable messages
+SELECT * FROM guestbook
+
